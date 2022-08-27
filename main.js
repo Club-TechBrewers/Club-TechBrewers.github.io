2b744bded0a7d2ddf6e0bc85d440af1c958f3471
Mrunank Pawar <mrunankpawar05@gmail.com>
2022-08-27 09:51:15 +0530

    added faq section

--- main.js
+++ main.js
@@ -3,3 +3,21 @@
 window.addEventListener('scroll', () => {
     document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0)
 })
+
+// show/ hide faq answer
+
+const faqs = document.querySelectorAll('.faq')
+
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open');
+
+        // change icon 
+        const icon = faq.querySelector('.faq__icon i');
+        if(icon.className === 'uil uil-plus'){
+            icon.className = 'uil uil-minus'
+        } else{
+            icon.className = 'uil uil-plus'
+        }
+    })
+})
