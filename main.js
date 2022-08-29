f412d82318559e1f7125c120531b662f9334de9d
Mrunank Pawar <mrunankpawar05@gmail.com>
2022-08-29 11:36:52 +0530

    added footer

--- main.js
+++ main.js
@@ -21,3 +21,25 @@ faqs.forEach(faq => {
         }
     })
 })
+
+// show/ hide nav menu
+
+const menu = document.querySelector(".nav__menu");
+const menuBtn = document.querySelector("#open-menu-btn");
+const closeBtn = document.querySelector("#close-menu-btn");
+
+menuBtn.addEventListener('click', () => {
+    menu.style.display = "flex";
+    closeBtn.style.display = "inline-block";
+    menuBtn.style.display = "none";
+})
+
+// close nav menu
+
+const closeNav = () => {
+    menu.style.display = "none";
+    closeBtn.style.display = "none";
+    menuBtn.style.display = "inline-block";
+}
+
+closeBtn.addEventListener('click', closeNav)
