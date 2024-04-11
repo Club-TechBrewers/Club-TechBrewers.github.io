de7c8eac39fc2ca68c651aadce482c8a88b23ede
Mrunank Pawar <65391854+mrunankpawar@users.noreply.github.com>
2024-04-10 19:59:53 -0700

    updated team

---
+++ techbrewersv3/src/App.jsx
@@ -0,0 +1,50 @@
+import "./App.css";
+
+// import { BackgroundImage } from './images/Landing Page Graphics.png';
+
+const App = () => {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello!</h1>
+      <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+          <div className="hidden sm:mb-8 sm:flex sm:justify-center">
+            <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+              🚨 Stay ahead of expiration dates with our handy notification system. {" "}
+            </div>
+          </div>
+          <div className="text-center">
+            <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+              Product Alert Assistance using ProQRuct
+            </h1>
+            <p className="mt-6 text-lg leading-8 text-gray-600">
+              Users can effortlessly stay ahead of expiring items, we are offering
+              a solution for managing expiration dates by implementing a
+              user-friendly notification system.
+            </p>
+            <div className="mt-10 flex items-center justify-center gap-x-6">
+              <a
+                href="#product-form"
+                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Get started
+              </a>
+            </div>
+          </div>
+          <div
+            className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+            aria-hidden="true"
+          >
+            <div
+              className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(20%+36rem)] sm:w-[32.1875rem]"
+              style={{
+                clipPath:
+                  "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+              }}
+            />
+          </div>
+        </div>
+    </>
+  );
+};
+
+export default App;
